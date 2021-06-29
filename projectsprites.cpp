49cd750bcc3552b6d984e97c8591c3a3fc164323
brandenap1 <86215396+brandenap1@users.noreply.github.com>
2021-06-29 18:11:12 -0500

    Add files via upload

--- projectsprites.cpp
+++ projectsprites.cpp
@@ -23,27 +23,53 @@ int main() {
 
     sf::RenderWindow window(sf::VideoMode(810, 1080), "bja955@bu.edu | cwgough@bu.edu");
     sf::Vector2f borderSize;
-    float borderEndx = 40.f;
-    float borderEndy = 40.f;
-    borderSize.x = window.getSize().x - 80;
-    borderSize.y = window.getSize().y - 80;
+    float borderEndx = 80.f;
+    float borderEndy = 80.f;
+    borderSize.x = window.getSize().x - 160;
+    borderSize.y = window.getSize().y - 160;
+
+    //spritesheet for char animations
     sf::Texture spritesheet;
+    spritesheet.loadFromFile("charspritescropped.png");
+    sf::IntRect SourceSprite(0,0, 210, 200);
+    sf::Sprite sprite(spritesheet,SourceSprite);
+    sprite.setPosition(310,1000);
+    sprite.setScale(0.8,0.8);
+    //spritesheet for game items
+    sf::Texture itemsheet;
+    itemsheet.loadFromFile("itemsspritesheetcropped.png");
+
+
+    //Items to be moved
+    // //clock item
+    sf::IntRect itemSourceSprite(30,185,90,90);
+    // //heart item
+    // sf::IntRect itemSourceSprite(145,185,90,90);
+    // //boulder item
+    // sf::IntRect itemSourceSprite(620,185,110,110);
+    // //stump item
+    // sf::IntRect itemSourceSprite(760,185,110,110);
+    //red scroll item (least points - slowest)
+    // sf::IntRect itemSourceSprite(920,175,70,105);
+    // //green scroll item (more points - faster)
+    // sf::IntRect itemSourceSprite(1005,175,70,105);
+    // //yellow scroll item (most points - fastest)
+    // sf::IntRect itemSourceSprite(1090,175,70,105);
+    
+    sf::Sprite itemsprite(itemsheet,itemSourceSprite);
+
+
+    //dimensions for gamescreen border
     sf::RectangleShape gameBorder(sf::Vector2f(borderSize.x,borderSize.y));
     gameBorder.setPosition(borderEndx, borderEndy);
     gameBorder.setFillColor(sf::Color::Black);
     gameBorder.setOutlineThickness(5);
     gameBorder.setOutlineColor(sf::Color::White);
-    spritesheet.loadFromFile("charspritescropped.png");
-    sf::IntRect SourceSprite(0,0, 210, 200);
-    sf::Sprite sprite(spritesheet,SourceSprite);
-    sprite.setScale(0.8,0.8);
+    //time for animation trigger
     sf::Clock clock;
-    // Animation charanimation(spritesheet, sf::Vector2u(6,0), 0.3f);
     std::cout.precision(18);
-    char direction = 'f';
     while (window.isOpen())
     {
-
         //Character moves
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
             sprite.move(2.f,0.f);
@@ -65,10 +91,10 @@ int main() {
         if (sprite.getPosition().y <= borderEndy ) {
             sprite.move(0.f,2.f);
         }
-        if (sprite.getPosition().x + sprite.getLocalBounds().width >= borderSize.x ) {
+        if (sprite.getPosition().x + sprite.getLocalBounds().width >= window.getSize().x ) {
             sprite.move(-2.f,0.f);
         }
-        if (sprite.getPosition().y + sprite.getLocalBounds().height >= borderSize.y ) {
+        if (sprite.getPosition().y + sprite.getLocalBounds().height >= window.getSize().y ) {
             sprite.move(0.f,-2.f);
         }
 
@@ -103,30 +129,12 @@ int main() {
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-
-            // left foot full extension
-             sf::IntRect lffe(0,0, 200, 200);
-             sf::Sprite charmove1(spritesheet,lffe);
-             // left foot half extension
-             sf::IntRect lfhe(220, 0, 210, 200);
-             sf::Sprite charmove2(spritesheet,lfhe);
-             // right foot forward
-             sf::IntRect rff(415,0, 210, 200);
-             sf::Sprite charmove3(spritesheet,rff);
-             // right foot half extension
-             sf::IntRect rfhe(645,0, 210, 200);
-             sf::Sprite charmove4(spritesheet,rfhe);
-             // right foot full extension
-             sf::IntRect rffe(870,0, 210, 200);
-             sf::Sprite charmove5(spritesheet,rffe);
-             // Idle
-             sf::IntRect idle(1090,0, 210, 200);
-             sf::Sprite charmove6(spritesheet, idle);
         }
      
         //draw sprites  
         window.clear(); 
-        window.draw(gameBorder);  
+        window.draw(gameBorder); 
+        window.draw(itemsprite); 
         window.draw(sprite);
         window.display();
     }
