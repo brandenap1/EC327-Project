a99ab8fbcf37e854b1611f7196f35a74a3b7cd81
brandenap1 <86215396+brandenap1@users.noreply.github.com>
2021-06-30 00:22:00 -0500

    Add files via upload
    Added collision detection

--- projectsprites.cpp
+++ projectsprites.cpp
@@ -1,3 +1,4 @@
+
 // Copyright 2021 Branden Applewhite bja955@bu.edu | Chris Gough cwgough@bu.edu
 
 
@@ -14,19 +15,41 @@
 #include <vector>
 #include <map>
 
-using std::cout; //Used for displaying time info in terminal 
-using std::cin;  //Adjusting time
-using std::string; //temps for time info
+using std::string;
+using std::to_string;
 
+bool CheckCollision(sf::Sprite player, sf::Sprite item) {
+    float proximity;
+    float basex;
+    float basey;
+    basex = (player.getPosition().x) - (item.getPosition().x);
+    basey = (player.getPosition().y) - (item.getPosition().y);
+    proximity = abs( sqrt( pow(basex,2) + pow(basey,2)));
+    proximity -= player.getLocalBounds().height / 2;
+    proximity -= item.getLocalBounds().height / 2;
+    //collision
+    if(proximity <= 0)
+        return true;
+    //no collision
+    return false;
+}
 
 int main() {
+  
+    // window
+    sf::RenderWindow window(sf::VideoMode(810, 1080), "bja955@bu.edu | cwgough@bu.edu | jsevilla@bu.edu");
 
-    sf::RenderWindow window(sf::VideoMode(810, 1080), "bja955@bu.edu | cwgough@bu.edu");
+    // dimensions for gamescreen border
     sf::Vector2f borderSize;
     float borderEndx = 80.f;
     float borderEndy = 80.f;
     borderSize.x = window.getSize().x - 160;
     borderSize.y = window.getSize().y - 160;
+    sf::RectangleShape gameBorder(sf::Vector2f(borderSize.x,borderSize.y));
+    gameBorder.setPosition(borderEndx, borderEndy);
+    gameBorder.setFillColor(sf::Color::Black);
+    gameBorder.setOutlineThickness(5);
+    gameBorder.setOutlineColor(sf::Color::White);
 
     //spritesheet for char animations
     sf::Texture spritesheet;
@@ -35,39 +58,69 @@ int main() {
     sf::Sprite sprite(spritesheet,SourceSprite);
     sprite.setPosition(310,1000);
     sprite.setScale(0.8,0.8);
+
     //spritesheet for game items
     sf::Texture itemsheet;
     itemsheet.loadFromFile("itemsspritesheetcropped.png");
 
-
+    std::vector<sf::Sprite> onscreenSprites;
     //Items to be moved
-    // //clock item
-    sf::IntRect itemSourceSprite(30,185,90,90);
-    // //heart item
-    // sf::IntRect itemSourceSprite(145,185,90,90);
-    // //boulder item
-    // sf::IntRect itemSourceSprite(620,185,110,110);
-    // //stump item
-    // sf::IntRect itemSourceSprite(760,185,110,110);
-    //red scroll item (least points - slowest)
-    // sf::IntRect itemSourceSprite(920,175,70,105);
-    // //green scroll item (more points - faster)
-    // sf::IntRect itemSourceSprite(1005,175,70,105);
-    // //yellow scroll item (most points - fastest)
-    // sf::IntRect itemSourceSprite(1090,175,70,105);
-    
-    sf::Sprite itemsprite(itemsheet,itemSourceSprite);
+    //clock item
+    sf::IntRect oclock(30,185,90,90);
+    sf::Sprite oclockSprite(itemsheet,oclock);
+    oclockSprite.setPosition(50,100);
+    onscreenSprites.push_back(oclockSprite);
+    //heart item
+    sf::IntRect heart(145,185,90,90);
+    sf::Sprite heartSprite(itemsheet,heart);
+    heartSprite.setPosition(170,100);
+    onscreenSprites.push_back(heartSprite);
+    //boulder item
+    sf::IntRect boulder(620,185,110,110);
+    sf::Sprite boulderSprite(itemsheet,boulder);
+    boulderSprite.setPosition(290,100);
+    onscreenSprites.push_back(boulderSprite);
+    //stump item
+    sf::IntRect stump(760,185,110,110);
+    sf::Sprite stumpSprite(itemsheet,stump);
+    stumpSprite.setPosition(410,100);
+    onscreenSprites.push_back(stumpSprite);    
+    // red scroll item (least points - slowest)
+    sf::IntRect rscroll(920,175,70,105);
+    sf::Sprite rscrollSprite(itemsheet,rscroll);
+    rscrollSprite.setPosition(530,100);
+    onscreenSprites.push_back(rscrollSprite);
+    //green scroll item (more points - faster)
+    sf::IntRect gscroll(1005,175,70,105);
+    sf::Sprite gscrollSprite(itemsheet,gscroll);
+    gscrollSprite.setPosition(740,100);
+    onscreenSprites.push_back(gscrollSprite);
+    //yellow scroll item (most points - fastest)
+    sf::IntRect yscroll(1090,175,70,105);
+    sf::Sprite yscrollSprite(itemsheet,yscroll);
+    yscrollSprite.setPosition(50,220);
+    onscreenSprites.push_back(yscrollSprite);
 
-
-    //dimensions for gamescreen border
-    sf::RectangleShape gameBorder(sf::Vector2f(borderSize.x,borderSize.y));
-    gameBorder.setPosition(borderEndx, borderEndy);
-    gameBorder.setFillColor(sf::Color::Black);
-    gameBorder.setOutlineThickness(5);
-    gameBorder.setOutlineColor(sf::Color::White);
     //time for animation trigger
     sf::Clock clock;
     std::cout.precision(18);
+  
+    // point counter
+    // border (may no longer be necessary)
+    // sf::RectangleShape pointCounter(sf::Vector2f(100, 50));
+    // pointCounter.setPosition(660, 50);
+    // pointCounter.setFillColor(sf::Color::Black);
+    // pointCounter.setOutlineThickness(5);
+    // pointCounter.setOutlineColor(sf::Color::White);
+    // points
+    sf::Font font;
+    font.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-BI.ttf");
+    sf::Text points;
+    points.setFont(font);
+    points.setCharacterSize(60);
+    points.setFillColor(sf::Color::Red);
+    points.setPosition(650, 5);
+
     while (window.isOpen())
     {
         //Character moves
@@ -83,7 +136,15 @@ int main() {
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
             sprite.move(0.f,2.f);
         }
+        for (auto it = onscreenSprites.begin(); it != onscreenSprites.end();) {
+            sf::Sprite curr = *it;
+            if (CheckCollision(sprite, curr)) {
+                it = onscreenSprites.erase(it);
+            }
 
+            else
+                it++; 
+        }
         //Border Collisions
         if (sprite.getPosition().x <=  borderEndx) {
             sprite.move(2.f,0.f);
@@ -102,21 +163,10 @@ int main() {
         //     case time 
         if (clock.getElapsedTime().asSeconds() > 0.1f) {
             if(SourceSprite.left == 1050) {
-                // SourceSprite.left -= 210;
-                // direction = 'b';
                 SourceSprite.left = 0;
             }
             else {
-                // if(direction == 'f') {
-                // SourceSprite.left += 210;
-                // }
-                // else if (direction == 'b') {
-                //     if(SourceSprite.left == 0){
-                        SourceSprite.left += 210;
-            //             direction = 'f';
-            //         }
-            //         SourceSprite.left -= 210;
-            //     }
+                SourceSprite.left += 210;
             }
 
             
@@ -124,18 +174,58 @@ int main() {
             clock.restart();
         }
 
+        // adding points
+        int collisions = 0;
+        // if (borders_collide == true)
+        //   collisions += (amount of points)
+        string numpoints = to_string(collisions);
+        if (numpoints.size() < 2)
+            numpoints = "0" + numpoints;
+        points.setString(numpoints);
+
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            // left foot full extension
+             sf::IntRect lffe(0,0, 200, 200);
+             sf::Sprite charmove1(spritesheet,lffe);
+             // left foot half extension
+             sf::IntRect lfhe(220, 0, 210, 200);
+             sf::Sprite charmove2(spritesheet,lfhe);
+             // right foot forward
+             sf::IntRect rff(415,0, 210, 200);
+             sf::Sprite charmove3(spritesheet,rff);
+             // right foot half extension
+             sf::IntRect rfhe(645,0, 210, 200);
+             sf::Sprite charmove4(spritesheet,rfhe);
+             // right foot full extension
+             sf::IntRect rffe(870,0, 210, 200);
+             sf::Sprite charmove5(spritesheet,rffe);
+             // Idle
+             sf::IntRect idle(1090,0, 210, 200);
+             sf::Sprite charmove6(spritesheet, idle);
+
         }
      
         //draw sprites  
         window.clear(); 
-        window.draw(gameBorder); 
-        window.draw(itemsprite); 
+        // window.draw(pointCounter);
+        window.draw(gameBorder);
+        for (auto it = onscreenSprites.begin(); it != onscreenSprites.end(); it++) {
+            window.draw(*it);
+        } 
+        // window.draw(oclockSprite);
+        // window.draw(heartSprite);
+        // window.draw(boulderSprite);
+        // window.draw(stumpSprite);
+        // window.draw(rscrollSprite);
+        // window.draw(gscrollSprite);
+        // window.draw(yscrollSprite);
         window.draw(sprite);
+        window.draw(points);
         window.display();
     }
     return 0;
